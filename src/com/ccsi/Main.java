da368ff023ef4d86430f1496c6927c2c868876a6
liugxiami <xiamiliu@gmail.com>
2016-10-19 23:07:10 -0500

    Update Main.java

--- src/com/ccsi/Main.java
+++ src/com/ccsi/Main.java
@@ -39,3 +39,89 @@ class Node{
         this.children=new LinkedList<>();
     }
 }
+
+//标准方法
+package com.ccsi;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    //1.recursion DFS+level
+    public static int depthSum(List<NestedInteger> nestedInteger) {
+        return dfs(nestedInteger,1);
+    }
+
+    public static int dfs(List<NestedInteger> nestedInteger, int dep) {
+        if(nestedInteger==null||nestedInteger.size()==0)return 0;
+
+        int sum=0;
+        for (int i = 0; i < nestedInteger.size(); i++) {
+            if(nestedInteger.get(i).isInteger()){
+                sum+=nestedInteger.get(i).getInteger()*dep;
+            }else{
+                sum+=dfs(nestedInteger.get(i).getList(),dep+1);
+            }
+        }
+        return sum;
+    }
+    //2.BFS+level  双queue
+    public static int depthSum1(List<NestedInteger> nestedInteger){
+        if(nestedInteger==null||nestedInteger.size()==0)return 0;
+
+        Queue<NestedInteger> data=new LinkedList<>();
+        Queue<Integer> level=new LinkedList<>();
+        int sum=0;
+
+        for(NestedInteger ni:nestedInteger){
+            data.offer(ni);
+            level.offer(1);
+        }
+
+        while(!data.isEmpty()){
+            NestedInteger curr=data.poll();
+            Integer currLever=level.poll();
+
+            if(curr.isInteger())sum+=curr.getInteger()*currLever;
+            else{
+                for(NestedInteger ni:curr.getList()){
+                    data.offer(ni);
+                    level.offer(currLever+1);
+                }
+            }
+        }
+        return sum;
+    }
+}
+
+interface NestedInteger{
+    public boolean isInteger();
+    public Integer getInteger();
+    public List<NestedInteger> getList();
+}
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+/*
+
+
+
+
+
